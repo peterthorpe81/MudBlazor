1a78ecf03f07088f9a2bb491615142d1b7c5ee03
Peter Thorpe <peterthorpe81@googlemail.com>
2024-01-11 15:44:53 +0000

    Revert "Revert "Fix for grid column hidden state""
    This reverts commit 2f4e552eedee428116c3e498c1d973052daf99c6.

---
+++ src/MudBlazor/Components/DataGrid/StateHandler.cs
@@ -0,0 +1,61 @@
+﻿// Copyright (c) MudBlazor 2021
+// MudBlazor licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace MudBlazor.Components.DataGrid
+{
+#nullable enable
+    internal class StateHandler<T>
+    {
+        private T? _lastValue;
+        private T? _value;
+        private EventCallback<T> _event;
+
+        internal T? Value
+        {
+            get => _value;
+            set
+            {
+                if (!EqualityComparer<T>.Default.Equals(_value, value))
+                {
+                    _value = value;
+                    if (_event.HasDelegate)
+                    {
+                        _lastValue = value;
+                        _event.InvokeAsync(value);
+                    }
+                }
+            }
+        }
+
+        internal StateHandler(T? state) 
+        {
+            _lastValue = _value = state;
+            _event = default;
+        }
+
+        internal StateHandler(T? state, EventCallback<T> ev)
+        {
+            _lastValue = _value = state;
+            _event = ev;
+        }
+
+        /// <summary>
+        /// Call this in OnParametersSetAsync to update state where external parameters have changed
+        /// </summary>
+        /// <param name="parameterValue"></param>
+        internal void SyncParameter(T parameterValue)
+        {
+            if (!EqualityComparer<T>.Default.Equals(_lastValue, parameterValue))
+                _lastValue = _value = parameterValue;
+        }
+    }
+#nullable disable
+}
